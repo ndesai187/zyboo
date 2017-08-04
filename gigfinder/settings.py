551eacf62528030ef8acc55f8cacbd03b9bdc5e5
Nirav Desai <ndesai187@gmail.com>
2017-08-04 21:02:57 +0800

    Created gigs app

--- gigfinder/settings.py
+++ gigfinder/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gigs',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'gigfinder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'gigfinder',
+        'USER': 'giguser',
+        'PASSWORD': 'giguser',
     }
 }
 
