201f831889f9e25ef6a2f0c480ef7fb9cbd41778
Nirav Desai <ndesai187@gmail.com>
2017-08-26 18:38:51 +0800

    Customised location widget

--- gigs/admin.py
+++ gigs/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from gigs.models import HappyPubs, PubEvent
+from django.forms import ModelForm
+from floppyforms.gis import PointWidget, BaseGMapWidget
+
+class CustomPointWidget(PointWidget, BaseGMapWidget):
+    class Media:
+        js = ('/static/floppyforms/js/MapWidget.js',)
+
+class HappyPubAdminForm(ModelForm):
+    class Meta:
+        model = HappyPubs
+        fields = ['name', 'location']
+        widgets = {
+            'location' : CustomPointWidget()
+        }
+
+class HappyPubAdmin(admin.ModelAdmin):
+    form = HappyPubAdminForm
 
 # Register your models here.
-admin.site.register(HappyPubs)
+admin.site.register(HappyPubs, HappyPubAdmin)
 admin.site.register(PubEvent)
