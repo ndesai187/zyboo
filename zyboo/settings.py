4c03646e5403be59012ddf010ac6265dccc9a986
Nirav Desai <ndesai187@gmail.com>
2017-10-08 12:38:09 +0800

    adding db config to settings

--- zyboo/settings.py
+++ zyboo/settings.py
@@ -77,14 +77,14 @@ WSGI_APPLICATION = 'zyboo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#        'NAME': 'zyboo',
-#        'USER': 'zyboouser',
-#        'PASSWORD': 'zyboouser',
-#    }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'zyboo',
+        'USER': 'zyboouser',
+        'PASSWORD': 'zyboouser',
+    }
+ }
 
 
 # Password validation
