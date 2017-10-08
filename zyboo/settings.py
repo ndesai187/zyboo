4f83667d21fc2be666cd7e96754c597b23aeffff
Nirav Desai <ndesai187@gmail.com>
2017-10-08 18:07:38 +0800

    update in settings for DB

--- zyboo/settings.py
+++ zyboo/settings.py
@@ -76,8 +76,7 @@ WSGI_APPLICATION = 'zyboo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=500)
 
 # for local DB
 # DATABASES = {
